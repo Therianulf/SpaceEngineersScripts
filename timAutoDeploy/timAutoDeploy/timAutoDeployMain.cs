5b31b8bb1be6a1c7eadec17e6a93da96625c8429
therian <therianlarson@gmail.com>
2017-01-20 21:31:00 -0800

    adding config vars, building setname functions.

--- timAutoDeploy/timAutoDeploy/timAutoDeployMain.cs
+++ timAutoDeploy/timAutoDeploy/timAutoDeployMain.cs
@@ -33,18 +33,50 @@ namespace timAutoDeploy
 
         //remove this line when import to SE
         IMyGridTerminalSystem GridTerminalSystem;
-
+        //******************************************************
         //start of code to be exported to SE
+        //******************************************************
+
+        //set ammoFirst to true if you wish for assemblers to assign ammo manufacturing before they assign plates.
+        // good for when you have limited assemblers in hostile territory and need constant ammo production.
+        public bool ammoFirst = false;
+        //set this to false if you don't want to assign the assembler component array names to the assemblers
+        public bool assignAssemblers = true;
+        //set this to false if you don't want tim to manage your refineries and arcs
+        public bool assignRefineries = true;
+        //set this to false if you don't want tim to assign your reactors uranium
+        public bool assignReactors = true;
+        //this is how much uranium to give to each reactor, make sure to have the trailing F if you change it, since thats what makes it a float in C# land
+        public float ReactorUraniumCount = 10.0F;
+        //set this to true if you want to have the assembler add modded items. get the modded item names by putting them into a cargo container that a running TIM script can see. Then using TIM's LCD component function, you should see the item listed on the LCD screen.
+        public bool addModComponentList = true; //todo set this to false for production
         public string[] assemblerComponentArray = { "SteelPlate", "CONSTRUCTION", "SMALLTUBE", "LARGETUBE", "COMPUTER", "MOTOR", "METALGRID", "POWERCELL", "BULLETPROOFGLASS", "DETECTOR", "DISPLAY", "REACTOR", "GIRDER", "SOLARCELL", "REACTOR", "INTERIORPLATE", "MEDICAL", "THRUST", "RADIOCOMMUNICATION", "GRAVITYGENERATOR", "EXPLOSIVES", "SUPERCONDUCTOR" };
         public string[] ammoArray = { "Missile200mm", "NATO_25x184mm", "NATO_5p56x45mm" };
+        //you can adjust these arrays if you wish to have it build items from different mods. by default it includes CSD autocannon and Battlecannon 
+        public string[] modComponentArray = { "Autocannon_Box","Autocannon_Box_Large","250shell","88shell","88hekc"};
         void Main(string argument){
-           List<IMyTerminalBlock> refineryBlocks = getRefineries();
-            foreach(IMyRefinery refinery in refineryBlocks){
-                string nameNow = refinery.CustomName;
-                nameNow = nameNow + " [TIM Ore]";
-                refinery.SetCustomName(nameNow);
+           
+        }
+
+        void setRefineryNames() {
+            List<IMyTerminalBlock> refineryBlocks = getRefineries();
+            foreach (IMyRefinery refinery in refineryBlocks){
+                string name = refinery.CustomName;
+                name = name + " [TIM Ore]";
+                refinery.SetCustomName(name);
             }
         }
+        void setAssemblerNames() {
+            List<IMyTerminalBlock> assemblerBlocks = getAssemblers();
+            int loopCounter = 0;
+            foreach (IMyAssembler assembler in assemblerBlocks) {
+                string name = assembler.CustomName;
+                name = name + "[TIM " + assemblerComponentArray[loopCounter] + "]";
+                assembler.SetCustomName(name);
+                loopCounter++;
+            }
+        }
+
 
         List<IMyTerminalBlock> getRefineries(){
             List<IMyTerminalBlock> refineryBlocks;
@@ -84,10 +116,11 @@ public void Save() {
     // 
     // This method is optional and can be removed if not
     // needed.
-}
+}
 
+        //******************************************************
         //end of code to be exported to SE
-
+        //******************************************************
 
 
     }
