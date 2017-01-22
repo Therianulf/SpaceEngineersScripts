caec50b3670a25e18f113380b37de48022e9475a
therian <therianlarson@gmail.com>
2017-01-21 18:12:06 -0800

    commiting release

--- timAutoDeploy/timAutoDeploy/timAutoDeployMain.cs
+++ timAutoDeploy/timAutoDeploy/timAutoDeployMain.cs
@@ -46,13 +46,22 @@ namespace timAutoDeploy {
         // Author: Therian
         // Version: 0.1
         // Purpose: To automatically setup TIM on new grids, without having to do 100 lines of data entry.
-        // Usage: Alter the 
-        // Features:
-        // Planned Features:
+        // Dependancies: Taleden's Inventory Manager [TIM]
+        // Before You Start:
+        // This script was designed to be ran when a ship is first built, and you want to install TIM.
+        // If you just want to save yourself some time, you should be able to run this completely default and save yourself quite a bit of typing.
+        // Make sure you don't have any connector or merge blocks attached if you don't want this to effect that grid as well.
+        // MAKE SURE YOU/FACTION OWN ALL THE BLOCKS ON THE GRID. 
+        // Usage: 
+        // 1. Copy and paste from start of code to end of code tags into your programming block, OR use the blueprint which will only include these.
+        // 2. Alter the below config variables to your preference.
+        // 3. Click Check code, if you're Configuration is acceptable it should compile successfully.
+        // 4. Click Remember and Exit
+        // 5. Click Run on the Programming block
+        // 6. If you assigned the assemblers, you still need to go to every assembler in the Production tab, and assign it the appropiate component, and then turn on repeat mode. (This is a space engineers limitation)
+        // Planned Features: setting cargo container groups, changing that cargo container group, changing cargo groups over antenna
         //******************************************************
         //******************************************************
-
-
 
         //******************************************************
         //******************************************************
@@ -88,7 +97,6 @@ namespace timAutoDeploy {
         //******************************************************
         //END OF CONFIG ZONE, careful changing below this
         //******************************************************
-
         //******************************************************
         //BE CAREFUL MESSING WITH THIS VARIABLE, IF YOU SET IT TO TRUE AFTER THE FIRST RUN, IT MAY OVERWRITE ANY CHANGES YOU HAVE MADE. YOU HAVE BEEN WARNED.
         public bool firstRun = true;
